31df356387b40a76e321d56e71e984898ed45aa5
Kirill M. <some@mail.com>
2025-08-12 23:33:55 +0300

    front: add login page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,24 +5,29 @@ import Login from './Login/Login'
 import './globalStyles.scss'
 import './fonts.scss'
 import Payment from './Payment/Payment'
+import LoginStatus from './Login/LoginStatus'
+import { IsLoginProvider } from './Login/IsLoginContext'
 
 const App: React.FC = () => {
     return (
         <>
             <BrowserRouter>
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/payment" element={<Payment />} />
-                    <Route path="/" element={<Persons />} />
-                    <Route
-                        path="*"
-                        element={
-                            <React.Fragment>
-                                <div>Nothing here</div>
-                            </React.Fragment>
-                        }
-                    />
-                </Routes>
+                <IsLoginProvider>
+                    <LoginStatus></LoginStatus>
+                    <Routes>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/payment" element={<Payment />} />
+                        <Route path="/" element={<Persons />} />
+                        <Route
+                            path="*"
+                            element={
+                                <React.Fragment>
+                                    <div>Nothing here</div>
+                                </React.Fragment>
+                            }
+                        />
+                    </Routes>
+                </IsLoginProvider>
             </BrowserRouter>
         </>
     )
